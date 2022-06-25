9f5a1934f0850b3d004ec017d62ad072047216fb
Karthik Reddy <ksk176326@gmail.com>
2022-06-25 09:30:47 +0200

    intial commit

---
+++ hh.py
@@ -0,0 +1,29 @@
+
+def countSubstr(str, n, b, a):
+
+    tot_count = 0
+
+
+    count_x = 0
+
+
+    for i in range(n):
+
+        if str[i] == b:
+            count_x += 1
+
+
+        if str[i] == a:
+            tot_count += count_x
+
+
+    return tot_count
+
+
+
+str = 'abbcaceghcak'
+n = len(str)
+b, a = 'a', 'a'
+print('Count =', countSubstr(str, n, b, a))
+
+
